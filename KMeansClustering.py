76bb22a3142ff4067feff2980f0ed658a8c07a5b
ShashankPhiske <shashankphiske@gmail.com>
2025-05-10 09:07:38 +0530

    ssp

---
+++ KMeansClustering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('clustering.csv')
+data.head()
+
+
+X = data[["LoanAmount","ApplicantIncome"]]
+#Visualise data points
+plt.scatter(X["ApplicantIncome"],X["LoanAmount"],c='black')
+plt.xlabel('AnnualIncome')
+plt.ylabel('Loan Amount (In Thousands)')
+plt.show()
+
+K=3
+
+# Select random observation as centroids
+Centroids = (X.sample(n=K))
+plt.scatter(X["ApplicantIncome"],X["LoanAmount"],c='black')
+plt.scatter(Centroids["ApplicantIncome"],Centroids["LoanAmount"],c='red')
+plt.xlabel('AnnualIncome')
+plt.ylabel('Loan Amount (In Thousands)')
+plt.show()
